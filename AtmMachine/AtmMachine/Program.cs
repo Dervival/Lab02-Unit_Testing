fb1e6958c5234bbdf61fca36b573d95461009ace
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-09 09:20:06 -0800

    Initialization of files

---
+++ AtmMachine/AtmMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtmMachine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
