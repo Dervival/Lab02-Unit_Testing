1f69b9c1f4e40e5765f2047bcfd954ecaed72ac6
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-09 09:44:11 -0800

    Added withdrawal functionality and basic tests

--- AtmMachine/AtmMachine/Program.cs
+++ AtmMachine/AtmMachine/Program.cs
@@ -4,9 +4,34 @@ namespace AtmMachine
 {
     public class Program
     {
+        public static decimal balance = 2000.00M;
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Withdraw(-10);
+            Withdraw(999.99M);
+            Withdraw(999);
+            Withdraw(10);
+        }
+
+        public static decimal Withdraw(decimal amount)
+        {
+            if(amount < 0)
+            {
+                Console.WriteLine("Invalid withdrawal amount of $" + amount + " was requested. Please make only non-negative withdrawals. Returning to main menu.");
+                return balance;
+            }
+            if(amount > balance)
+            {
+                Console.WriteLine("Insufficient funds. A withdrawal of $" + amount + " was requested, but there is only $" + balance + " in the account. Returning to main menu.");
+                return balance;
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("Withdrawal of $" + amount + " was successful. You have $" + balance + " left in this account.");
+                return balance;
+            }
         }
     }
 }
--- AtmMachine/XUnitTestProject1/UnitTest1.cs
+++ AtmMachine/XUnitTestProject1/UnitTest1.cs
@@ -4,14 +4,37 @@ using AtmMachine;
 
 namespace AtmXUnitTests
 {
-    public class UnitTest1
+    public class AtmXUnitTests
     {
         [Fact]
-        public void Test1()
+        public void WithdrawFromAccountChangesBalance()
         {
-            string[] arguments = { };
-            Program.Main(arguments);
-            Assert.Equal(1, 1);
+            //arrange
+            Program.balance = 200.00M;
+            //act
+            Program.Withdraw(100.00M);
+            //assert
+            Assert.Equal(100.00M, Program.balance);
+        }
+        [Fact]
+        public void WithdrawalRejectsOverdrafts()
+        {
+            //arrange
+            Program.balance = 100.00M;
+            //act
+            Program.Withdraw(200.00M);
+            //assert
+            Assert.Equal(100.00M, Program.balance);
+        }
+        [Fact]
+        public void WithdrawalRejectsNegativeInput()
+        {
+            //arrange
+            Program.balance = 100.00M;
+            //act
+            Program.Withdraw(-50.00M);
+            //assert
+            Assert.Equal(100.00M, Program.balance);
         }
     }
 }
